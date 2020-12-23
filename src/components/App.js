6d5f8964339e5c4811dc81615026628d5442deb0
김정민 <dsaqaz421@naver.com>
2020-12-23 17:42:17 +0900

    앱 기본 설정 중 ...

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import AppRouter from "components/Router";
 import {authService} from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);                // 초기 설정 확인
+  const [isLoggedIn, setIsLoggedIn] = useState(false);    // 로그인 처리용
+
+  useEffect(() => {
+    authService.onAuthStateChanged( (user) => {   // onAuthStateChanged를 통해 user값을 받아왔을때
+      if(user) setIsLoggedIn(true);               // 정보가 있으면 로그인처리
+      else     setIsLoggedIn(false);              // 없으면 로그인 상태 아님
+      setInit(true);                              // 로그인 초기 설정 완료
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>       
-      </header>
-    </div>
+    <>
+      {init ? <AppRouter isLoggedIn = {isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; NaceBook {new Date().getFullYear}</footer>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import fbase from "fbase";
 
 ReactDOM.render(
   <React.StrictMode>
